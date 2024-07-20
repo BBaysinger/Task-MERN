4acd5f8c643d3279766eda0457c788b2d1ebb7bf
Bradley Baysinger <bradleybaysinger@gmail.com>
2024-07-20 07:12:32 -0700

    Register functional.

---
+++ frontend/src/components/Spinner.js
@@ -0,0 +1,9 @@
+const Spinner = () => {
+    return (
+        <div className="laodingSpinnerContainer">
+            <div className="loadingSpinner" />
+        </div>
+    )
+};
+
+export default Spinner;
