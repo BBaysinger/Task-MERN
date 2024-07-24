1f4030f16f59b69349272a10a11c8f955b0051e7
Bradley Baysinger <bradleybaysinger@gmail.com>
2024-07-23 23:48:44 -0700

    Adding dashboard.

---
+++ frontend/src/features/tasks/taskSlice.js
@@ -0,0 +1,34 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import taskService from './taskService';
+
+const initialState = {
+  tasks: [],
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  message: '',
+}
+
+export const createTask = createAsyncThunk(
+  'tasks/create',
+  async (taskData, thunkAPI) => {
+    try {
+      const token = thunkAPI.getState().auth.user.token;
+      return await taskService.createTask(taskData, token);
+    } catch (error) {
+      const message = (error.response && error.response.data && error.reponse.data.message) || error.message || error.toString();
+      return thunkAPI.rejectWithValue(message);
+    }
+  } 
+);
+
+export const taskSlice = createSlice({
+  name: 'task',
+  initialState,
+  reducers: {
+    reset: state => initialState
+  }
+});
+
+export const { reset } = taskSlice.actions;
+export default taskSlice.reducer;
